2064e430aab4112d0160a40ee335feac40f653ee
nate-kirschner <77256794+nate-kirschner@users.noreply.github.com>
2022-04-07 22:23:25 -0400

    leaderboard page shows results

--- src/components/Leaderboard/BestBallLeaderboard.js
+++ src/components/Leaderboard/BestBallLeaderboard.js
@@ -7,21 +7,36 @@ import LeagueCard from "../Leagues/LeagueCard";
 export default function BestBallLeaderboard() {
 
     const [leagueId, setLeagueId] = useState();
+    const [tournamentList, setTournamentList] = useState();
 
     useEffect(() => {
         axios.post(config.url + "/get-league-name", { leagueName: "BestBallTourn" }).then(resp => {
             setLeagueId(resp.data[0].id)
         })
+        const tournamentNames = ["Masters Tournament", "PGA Championship", "U.S. Open", "Open Championship"]
+        axios.post(config.url + "/get-tournament-ids", { tournamentNames }).then(resp => {
+            const tournamentListTemp = {};
+            resp.data.forEach(tourn => {
+                tournamentListTemp[tourn.id] = tourn.tournament_name
+            })
+            let count = -1;
+            tournamentNames.forEach(name => {
+                if (!Object.values(tournamentListTemp).includes(name)) {
+                    tournamentListTemp[count] = name;
+                    count++;
+                }
+            })
+            setTournamentList(tournamentListTemp)
+        })
     }, [])
 
     return (
         <div className="leaderboard mainPage">
-            {/* <h2 /> */}
-            {leagueId && (
+            {leagueId && tournamentList && (
                 <LeagueCard 
                     leagueId={leagueId} 
                     leagueName="Best Ball Tournament" 
-                    tournamentList={{1: 'Masters Tournament', 2: 'PGA Championship', 3: 'U.S. Open', 4: 'Open Championship'}}
+                    tournamentList={tournamentList}
                 />
             )}
         </div>
--- src/components/Leagues/LeagueCard.js
+++ src/components/Leagues/LeagueCard.js
@@ -7,15 +7,18 @@ import '../../styles/leagueCard.scss';
 export default function LeagueCard({ leagueId, leagueName, tournamentList }) {
 
     const [leagueMemberInfo, setLeagueMemberInfo] = useState();
+    
 
     useEffect(() => {
-        const body = {
-            leagueTournamentIds: Object.keys(tournamentList).map(Number),
-            leagueId,
+        if (tournamentList !== null) {
+            const body = {
+                leagueTournamentIds: Object.keys(tournamentList).map(Number),
+                leagueId,
+            }
+            axios.post(config.url + "/get-league-info", body).then(resp => {
+                setLeagueMemberInfo(resp.data);
+            })
         }
-        axios.post(config.url + "/get-league-info", body).then(resp => {
-            setLeagueMemberInfo(resp.data);
-        })
     }, [leagueId, leagueName, tournamentList])
 
     return (
@@ -29,7 +32,9 @@ export default function LeagueCard({ leagueId, leagueName, tournamentList }) {
                         style={{
                             width: `${100 / (Object.keys(tournamentList).length + 2)}%`
                         }}
-                    ><h4>Member</h4></div>
+                    >
+                        <h4>Member</h4>
+                    </div>
                     {
                         Object.keys(tournamentList).map(tourn => {
                             return (
@@ -46,50 +51,52 @@ export default function LeagueCard({ leagueId, leagueName, tournamentList }) {
                         style={{
                             width: `${100 / (Object.keys(tournamentList).length + 2)}%`
                         }}
-                    ><h4>Total</h4></div>
+                    >
+                        <h4>Total</h4>
+                    </div>
                 </div>
-                {
-                    leagueMemberInfo && leagueMemberInfo.map(user => {
-                        let sum = 0;
-                        return (
-                            <div className="playerCardRow row">
-                                <div className="playerCardData"
-                                    style={{
-                                        width: `${100 / (Object.keys(tournamentList).length + 2)}%`
-                                    }}
-                                ><h4>{user.username}</h4></div>
-                                {
-                                    Object.keys(tournamentList).map(tourn => {
-                                        let data = leagueMemberInfo.find(leagueUser => leagueUser.username === user.username).rosters.find(roster => roster.tournament_id == tourn)
-                                        if (data) {
-                                            sum += data.bestball_total;
-                                        }
-                                        return (
-                                            <div className="playerCardData"
-                                                style={{
-                                                    width: `${100 / (Object.keys(tournamentList).length + 2)}%`
-                                                }}
-                                            >
-                                                <h4>
-                                                    {
-                                                        data ? data.bestball_total : "-"
-                                                    }
-                                                </h4>
-                                            </div>
-                                        )
-                                    })
-                                }
-                                <div className="playerCardData"
-                                    style={{
-                                        width: `${100 / (Object.keys(tournamentList).length + 2)}%`
-                                    }}
-                                ><h4>{sum}</h4></div>
+                    {
+                        leagueMemberInfo && leagueMemberInfo.map(user => {
+                            let sum = 0;
+                            return (
+                                <div className="playerCardRow row">
+                                    <div className="playerCardData"
+                                        style={{
+                                            width: `${100 / (Object.keys(tournamentList).length + 2)}%`
+                                        }}
+                                    ><h4>{user.username}</h4></div>
+                                    {
+                                        Object.keys(tournamentList).map(tourn => {
+                                            let data = leagueMemberInfo.find(leagueUser => leagueUser.username == user.username).rosters.find(roster => roster.tournament_id == tourn)
+                                            if (data) {
+                                                sum += data.bestball_total;
+                                            }
+                                            return (
+                                                <div className="playerCardData"
+                                                    style={{
+                                                        width: `${100 / (Object.keys(tournamentList).length + 2)}%`
+                                                    }}
+                                                >
+                                                    <h4>
+                                                        {
+                                                            data ? data.bestball_total : "-"
+                                                        }
+                                                    </h4>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                    <div className="playerCardData"
+                                        style={{
+                                            width: `${100 / (Object.keys(tournamentList).length + 2)}%`
+                                        }}
+                                    ><h4>{sum}</h4></div>
 
-                            </div>
-                        )
-                    })
-                }
-            </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
         </div>
     )
 }
